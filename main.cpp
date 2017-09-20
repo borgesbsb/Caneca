0476c2f405d8e05883712b86fa0ec95d01b2f2cf
BENJAMIN DA SILVA BORGES <borges.bnjamin@gmail.com>
2017-09-20 00:12:53 +0000

    Apenas um commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-
-
-
-
-
+#include <cstdlib>
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    
+    string teste;
+    
+    cout << "Digite qualquer coisa pra mim..." << std::endl;    
+    cin >> teste;
+    
+    cout << teste;
+    
     return 0;
 }
